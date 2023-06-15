aa9a9bb84b9f8224d4db51c8b091f7c55cb4ad8e
Hissitissam <tissam.hisssi@gmail.com>
2023-06-15 08:12:27 -0700

    task4

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main: entry point
-* return: 0
-*/
+ * main  entry point
+ *
+ * return: 0
+ */
 int main (void)
 {
-	puts("Programming is like building a multilingual puzzle\n") ; 
+	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0);
 }
