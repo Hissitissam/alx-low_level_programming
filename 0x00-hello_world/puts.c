b588376eb5997a1565098e197ab5db7824f772a1
Hissitissam <tissam.hisssi@gmail.com>
2023-06-15 08:16:12 -0700

    task4

--- 0x00-hello_world/puts.c
+++ 0x00-hello_world/puts.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main  entry point
  *
  * return: 0
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle"); 
-	return (0);
+	return(0);
 }
 
 
