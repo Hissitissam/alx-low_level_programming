b88d0b43b35ae9e437e1122da6ef5cbadcf1d423
Hissitissam <tissam.hisssi@gmail.com>
2023-07-27 04:24:54 -0700

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 
 /**
  * list_len - returns the number of elements in a linked list
@@ -19,5 +19,4 @@ size_t list_len(const list_t *h)
 	}
 	return (n);
 }
-
 
