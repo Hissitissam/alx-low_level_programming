5b43485a04574982f5f50a5f95df9bee388d8e45
Hissitissam <tissam.hisssi@gmail.com>
2023-07-31 12:09:53 -0700

    task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,12 +10,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t num_node = 0
 
-	while (h != NULL)
+	while (h)
 	{
+		printf("%d\n", h->n);
 		num_node++;
 		h = h->next;
 	}
-	printf("%d\n", h->n);
 
 	return (num_node);
 }
